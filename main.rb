ad26d29fe2e98f328617f6612b721a8b4e538cfd
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-07-14 12:36:05 +0200

    Add pick_random_word method

--- main.rb
+++ main.rb
@@ -1,1 +1,7 @@
 puts "Let's play Hangman"
+
+def pick_random_word
+  File.readlines("google-10000-english-no-swears.txt").select { |word| word.length > 4 && word.length < 13 }.sample
+end
+
+puts pick_random_word
