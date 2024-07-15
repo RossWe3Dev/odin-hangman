fa556e8403dee2e970ec0a56efc796e673d7365d
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-07-15 13:01:43 +0200

    Edit main.rb, it now asks before using saved file
    If saved_game.json is empty the game proceeds as normal. If not the
    player is asked if they want to use the saved file or not by the
    restore_saved_file method.

--- main.rb
+++ main.rb
@@ -2,18 +2,41 @@ require "colorize"
 require_relative "lib/game"
 
 def play_game
-  puts "Let's play Hangman".colorize(:yellow)
-  hangman = Game.new
+  puts "Let's play Hangman!".colorize(:yellow)
+  hangman = check_saved_file
   hangman.play
   play_again
 end
 
 def play_again
-  puts "\nPress 'y' to play again :) [y/quit]".colorize(:cyan)
+  puts "\nPress 'y' to continue playing :) [y / quit]".colorize(:cyan)
   if gets.chomp.downcase == "y"
     play_game
   else
     puts "Thanks for playing!".colorize(:magenta)
+  end
+end
+
+def check_saved_file
+  if File.empty?("saved_game.json")
+    Game.new
+  else
+    puts "There is a saved game, would you like to continue it? [yes / no]".colorize(:cyan)
+    restore_saved_file?
+  end
+end
+
+def restore_saved_file?
+  loop do
+    answer = gets.chomp.downcase.strip
+    if answer == "yes"
+      return Game.new
+    elsif answer == "no"
+      File.open("saved_game.json", "w") { |file| file.truncate(0) }
+      return Game.new
+    else
+      puts "Invalid input. Please answer 'yes' or 'no'!".colorize(:red)
+    end
   end
 end
 
